08e3569dc75c095dd9f01f3f447cb657d4ea74b6
Neel Smith <dnsmith.neel@gmail.com>
2021-05-13 14:57:54 -0400

    Add script to make complete PDF

---
+++ makepdf.sh
@@ -0,0 +1,3 @@
+# Requires pandoc and xelatex.
+PANDOC=`which pandoc`
+$PANDOC  --output "thesis.pdf" --variable mainfont="Gentium"  --template="typesetting/style/template.tex" --include-in-header="typesetting/style/preamble.tex"                 --toc --variable=fontsize:12pt --variable=documentclass:report --pdf-engine=xelatex "Chapters"/*.md  "Chapters/metadata.yml" --bibliography="Chapters/references.bib" --citeproc --csl="typesetting/style/ref_format.csl"  --number-sections --verbose
